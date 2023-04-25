17590a227eb39d851c193ce2fd947d152be5c2b7
joel-chad <joelchadambuka@gmail.com>
2023-04-25 02:55:52 +0200

    navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import Link from "next/link";
+// import { useRouter } from "next/router";
+// import userData from "@constants/data";
+
+
+
+function Navbar() {
+    return ( 
+          <nav className="p-4 mb-12 flex justify-between">
+            <Link href='/'><h1 className="font-burtons sm:text-base hover:text-teal-200 text-xl">joelchadambuka</h1></Link>
+            <ul className="flex items-center">
+              {/* <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li> */}
+              <li>
+                <Link
+                  className=" px-2 py-2 border-none rounded-md hover:text-teal-200"
+                  href="/aboutme"
+                >
+                    About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className=" px-2 py-2 border-none rounded-md hover:text-teal-200"
+                  href="/projects"
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className=" px-2 py-2 border-none rounded-md ml-8 hover:text-teal-200"
+                  href='/contact'
+                >
+                    Contact Us
+                </Link>
+              </li>
+              <li>
+                <Link className='bg-gradient-to-r from-cyan-500 text- to-teal-500 hover:bg-gradient-to-l  px-4 py-2 border-none rounded-md ml-8' href="#"> 
+                  Register
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          
+     );
+}
+
+export default Navbar;
