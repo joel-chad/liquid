9c789076756de18ae098b2e1416592f6f0019bbb
joel-chad <joelchadambuka@gmail.com>
2023-04-28 02:29:10 +0200

    push

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import Image from 'next/image'
+import Carousel from '../components/Carousel'
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className='font-semibold'>Home Page</h1>
-      <p>Client lands here first. They get basic information that includes about what the business does and the steps they can 
-        take to sign up.
-      </p>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <Carousel/>
     </main>
   )
 }
